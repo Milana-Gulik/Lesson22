059c095f56fc2e12556f2f30b9ad7f478781ac0d
Milana-Gulik <milanagulik@mail.ru>
2022-10-11 11:45:26 +0300

    Initial commit

---
+++ src/by/itstep/gulik/controller/Main.java
@@ -0,0 +1,13 @@
+package by.itstep.gulik.controller;
+
+import by.itstep.gulik.model.entity.Student;
+import by.itstep.gulik.view.Printer;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        Printer.print("testing...");
+
+    }
+}
